0653dff329abb4d361580bd9adf0719b448283ec
Robert Weikert <robertw3256@gmail.com>
2019-12-01 00:13:45 +0100

    Update pab07

--- src/PAB07/PAB07zad01.groovy
+++ src/PAB07/PAB07zad01.groovy
@@ -1,9 +1,21 @@
 package PAB07
 
-def projektyFile = new File('E:\\Pliki\\IJ Workspace\\PAB01\\src\\PAB07\\Projekty')
+def empsMap = [:]
+def projektyDuzeFile = new File('projekyDuze')
+def projektyFile = new File('E:\\Pliki\\IJ Workspace\\PAB01\\src\\PAB07\\Projekty').eachLine { line ->
+    def tokens = line.tokenize('\t')
+    def project = tokens[0]
+    def emps = tokens[1..tokens.size() - 1]
+    emps.each { emp ->
+        if (empsMap.containsKey(emp))
+            empsMap.get(emp).add(project)
+        else {
+            def nList = [project]
+            empsMap.put(emp, nList)
+        }
+    }
+    if (emps.size() > 3) projektyDuzeFile << "$project: $emps \n"
+}
 
-def projektyContent = projektyFile.text
-
-new File('projekyDuze') << projektyContent.findAll('(/d+)\t(((/d+)\t)+)', { all, project, persons ->
-    persons.size > 3
-})
+def programisciFile = new File('programisci')
+empsMap.each { key, value -> programisciFile << "Dev: $key \nProjekty: $value \n\n" }
