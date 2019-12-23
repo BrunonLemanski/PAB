0b548ff0e674a27b46d642d73f8fe6c44f0e7677
Robert Weikert <robertw3256@gmail.com>
2019-12-23 20:21:25 +0100

    Update PAB08zad01.groovy

--- src/PAB08/PAB08zad01.groovy
+++ src/PAB08/PAB08zad01.groovy
@@ -1,2 +1,6 @@
 package PAB08
 
+import static javax.swing.JOptionPane.*
+
+println showInputDialog('Enter 5 numbers').tokenize()*.toInteger()*.multiply(2)
+//println showInputDialog('Enter 5 numbers').tokenize()*.toInteger().collect() {it*2}
